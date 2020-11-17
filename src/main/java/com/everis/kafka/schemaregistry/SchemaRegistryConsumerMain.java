86686ef77fd26c262ee561ae6fb850970e8b5d11
Carlos Manuel Chanta <carloschanta152@gmail.com>
2020-11-17 03:12:07 -0500

    subir proyecto

---
+++ src/main/java/com/everis/kafka/schemaregistry/SchemaRegistryConsumerMain.java
@@ -0,0 +1,9 @@
+package com.everis.kafka.schemaregistry;
+
+public class SchemaRegistryConsumerMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
